901aff884cdce15eb5e7ac461bb7c9ae21695a5c
Clive Freeman <clivemeister@gmail.com>
2018-07-12 17:06:38 +0100

    Tidying up stuff

--- account.py
+++ account.py
@@ -3,10 +3,15 @@ from blockchain import recordBlockchainTransaction
 class Account():
 
     def __init__(self, name, initialBalance=0.0):
+        """Create account with the given name and starting balance
+        """
         self.name = name
         self.balance = initialBalance
 
     def withdraw(self, amount):
+        """Reduce balance for this account by given amount
+           Notify the blockchain object as we do this
+        """   
         self.balance -= amount
         recordBlockchainTransaction(self.name,"debit",amount)
         return self.balance
